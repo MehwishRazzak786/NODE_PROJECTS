effc747e92f1534b16c9e5b998bb1b86fd24e221
MehwishRazzak786 <a.razzakmehwish786786@gmail.com>
2024-04-18 04:58:28 +0500

    upload project05

---
+++ project05_word_counter/index.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+// Welcome message
+console.log(chalk.bold.underline.cyan `\n \t Welcome To Word-Count Application\n`);
+const userAns = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: chalk.bold.underline.green("Enter Your Sentence To Count The Word : ")
+    }
+]);
+const words = userAns.sentence.trim().split(" ");
+// print the array of words to the console
+// console.log(words);
+// print the word count of the sentence to the console
+console.log(chalk.bold.underline.green(`\nYour sentence word count is ${words.length}`));
